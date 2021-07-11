d85e1ab865f50970566e4e3f0104321ffe65a6d9
Josh Farr <sigsegv@quantumfish.com>
2021-07-11 11:29:08 -0700

    start window

--- ui/window.go
+++ ui/window.go
@@ -1,1 +1,39 @@
 package ui
+
+import "github.com/gotk3/gotk3/gtk"
+
+type Window struct {
+	Ref gtk.Window
+}
+
+func NewWindow() (*Window, error) {
+	window := &Window{}
+	return window, nil
+}
+
+func (window *Window) Init() error {
+	gtk.Init(nil)
+
+	var err error
+	window.Ref, err = gtk.windowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		return err
+	}
+
+	window.Ref.SetDefaultSize(800, 600)
+	window.Ref.SetTitle("shot-capture")
+	window.Ref.Connect("destroy", func() {
+		gtk.MainQuit()
+	}
+	return nil
+}
+
+func (window *Window) Destroy() error {
+	return nil
+}
+
+func (window *Window) Run() error {
+	window.Ref.ShowAll()
+	gtk.Main()
+	return nil
+}
