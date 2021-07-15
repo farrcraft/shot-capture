adfcaca8f3e71b1904b5e738458693010910c208
Josh Farr <sigsegv@quantumfish.com>
2021-07-15 00:11:41 -0700

    option for arm package

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -ex
 
-GOLANG_VERSION=1.16.5
+GOLANG_VERSION=1.16.6
+
+# pi is an ARMv7 so closest working go binaries are "v6l"
+PLATFORM=amd64
 
 pushd .
 
 cd /tmp/
 
-curl -LO https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+curl -LO https://golang.org/dl/go$GOLANG_VERSION.linux-$PLATFORM.tar.gz
 
 if [ -d /usr/local/go ]; then
     sudo rm -rf /usr/local/go
 fi
 
-sudo tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$GOLANG_VERSION.linux-$PLATFORM.tar.gz
 
 popd
 
